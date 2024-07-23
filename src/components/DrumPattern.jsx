c3a4df2399faef5e92bef3561a5da6642a42a59c
Cameron Adrian <52980873+cameron-adrian@users.noreply.github.com>
2024-07-22 21:29:34 -0400

    chore: Improve DrumPattern component layout and add placeholder for empty pattern

--- src/components/DrumPattern.jsx
+++ src/components/DrumPattern.jsx
@@ -4,12 +4,12 @@ function DrumPattern({ pattern }) {
   return (
     <div className="drum-pattern">
       <h2>Your Pattern:</h2>
-      <p className="pattern">{pattern.join(" ")}</p>
+      <p className="pattern">
+        {pattern != 0 ? pattern.join(" ") : "\u00A0\u00A0\u00A0"}
+      </p>
       {/* TODO: fill this p tag with &nbsp characters to make sure it's a certain size before the pattern comes 
-      TODO: center the pattern characters
       TODO: pattern character improvements: add bold, colors, etc to help differentiate them visually
       TODO: button to randomize the theme colors
-      TODO: create new component to hold all parameter changey checkboxes and stuff
       TODO: checkbox to toggle accents
       TODO: checkbox to toggle ghost notes
       TODO: checkbox to toggle kick drum
