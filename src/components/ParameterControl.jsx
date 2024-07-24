a622b24b2972559e296d89ad9e19d9c0cf88ebef
Cameron Adrian <52980873+cameron-adrian@users.noreply.github.com>
2024-07-24 13:03:43 -0400

    chore: Update parameter control layout and styling

--- src/components/ParameterControl.jsx
+++ src/components/ParameterControl.jsx
@@ -6,33 +6,39 @@ function ParameterControl(props) {
 
   return (
     <div className="input-group">
-      <label htmlFor="patternLength">Pattern Length: {patternLength}</label>
-      <input
-        className="slider"
-        type="range"
-        id="patternLength"
-        value={patternLength}
-        onChange={(e) =>
-          setPatternLength(
-            Math.min(16, Math.max(1, parseInt(e.target.value) || 1))
-          )
-        }
-        min="1"
-        max="16"
-      />
-      <label htmlFor="tempo">Tempo (BPM): {tempo}</label>
-      <input
-        className="slider"
-        type="range"
-        id="tempo"
-        value={tempo}
-        onChange={(e) =>
-          setTempo(Math.min(240, Math.max(12, parseInt(e.target.value) || 120)))
-        }
-        min="12"
-        max="240"
-        step="1"
-      />
+      <div>
+        <label htmlFor="patternLength">Pattern Length: {patternLength}</label>
+        <input
+          className="slider"
+          type="range"
+          id="patternLength"
+          value={patternLength}
+          onChange={(e) =>
+            setPatternLength(
+              Math.min(16, Math.max(1, parseInt(e.target.value) || 1))
+            )
+          }
+          min="1"
+          max="16"
+        />
+      </div>
+      <div>
+        <label htmlFor="tempo">Tempo (BPM):</label>
+        <input
+          className="slider"
+          type="number"
+          id="tempo"
+          value={tempo}
+          onChange={(e) =>
+            setTempo(
+              Math.min(240, Math.max(12, parseInt(e.target.value) || 120))
+            )
+          }
+          min="12"
+          max="240"
+          step="1"
+        />
+      </div>
     </div>
   );
 }
