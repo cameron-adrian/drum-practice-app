5b197c5ed3db61ab94cd981e3f3590b5e31f3ce7
Cameron Adrian <52980873+cameron-adrian@users.noreply.github.com>
2024-07-25 17:39:24 -0400

    chore: Add todo file with todos

---
+++ TODO.md
@@ -0,0 +1,33 @@
+# TODO List
+
+## High Priority
+
+- [ ] Better tooltips
+- [ ] Add sidebar thing
+- [ ]
+
+## Medium Priority
+
+- [ ] Random theme button
+- [ ] Copy pattern button
+- [ ] Add highlighting to each letter as it plays
+
+I would actually have to update the pattern array each time right? Like to update the state of the whole array at the position of the current letter being played
+I'd need to import the setPattern function from App.jsx and pass it down as a prop.
+
+I'd could maybe use a useEffect to update the pattern array each time a sound is played but the pattern array would have to be broken up into individual elements and probably displayed as a list of spans or something
+
+- [ ] Optimize playback in browser so it's even and doesn't speed up or slow down
+- [ ] Pattern character improvements: add bold, colors, etc to help differentiate them visually. Each one needs a CSS class
+
+      Checkboxes
+
+- [ ] checkbox to toggle accents
+- [ ] checkbox to toggle ghost notes
+- [ ] checkbox to toggle kick drum
+
+## Low Priority
+
+- [ ]
+- [ ]
+- [ ]
--- src/components/DrumPattern.jsx
+++ src/components/DrumPattern.jsx
@@ -10,17 +10,12 @@ function DrumPattern({ pattern, tempo }) {
     ŗ: "/sounds/snare-soft.mp3",
     ļ: "/sounds/snare-soft.mp3",
   };
-  // TODO: Add highlighting to each letter as it plays
-  // I would actually have to update the pattern array each time right? Like to update the state of the whole array at the position of the current letter being played
-  // I'd need to import the setPattern function from App.jsx and pass it down as a prop
-  // I'd could maybe use a useEffect to update the pattern array each time a sound is played
-  // but the pattern array would have to be broken up into individual elements and probably displayed as a list of spans or something
+
   const playSound = (sound) => {
     const audio = new Audio(drumSounds[sound]);
     audio.play();
   };
 
-  // TODO: optimize playback so it's even and doesn't speed up or slow down
   const playPattern = () => {
     const interval = 60000 / tempo;
     pattern.forEach((sound, index) => {
@@ -35,13 +30,6 @@ function DrumPattern({ pattern, tempo }) {
         {pattern != 0 ? pattern.join(" ") : "\u00A0\u00A0\n\u00A0\u00A0"}
       </p>
       <button onClick={playPattern}>Play Pattern</button>
-      {/*  
-      TODO: Pattern character improvements: add bold, colors, etc to help differentiate them visually. Each one needs a css class
-      TODO: button to randomize the theme colors
-      TODO: checkbox to toggle accents
-      TODO: checkbox to toggle ghost notes
-      TODO: checkbox to toggle kick drum
-      TODO: copy pattern button*/}
     </div>
   );
 }
